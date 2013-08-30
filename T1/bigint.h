638766de205a36c3345ffd6b352be92373c7b7ac
Carlos Mattoso <calmattoso@hotmail.com>
2013-08-30 18:59:48 -0300

    Added bigint library files (.h and .c).\

---
+++ T1/bigint.h
@@ -0,0 +1,42 @@
+#ifndef BIGINT_H
+#define BIGINT_H
+
+/* Atribuição */
+
+/* res = val (extensão com sinal) */
+void big_val(int val);
+
+/* res = uval (extensão sem sinal) */
+void big_uval(unsigned int uval);
+
+/* Operacoes aritméticas */
+
+/* res = a + b */
+void big_sum (BigInt res, BigInt a, BigInt b);
+
+/* res = a - b */
+void big_sub (BigInt res, BigInt a, BigInt b);
+
+/* res = a * b (com sinal) */
+void big_mul (BigInt res, BigInt a, BigInt b);
+
+/* res = a * b (sem sinal) */
+void big_umul (BigInt res, BigInt a, BigInt b);
+
+/* Operacoes de deslocamento */
+
+/* res = a << n */
+void big_shl (BigInt res, BigInt a, int n);
+
+/* res = a >> n (lógico) */
+void big_shr (BigInt res, BigInt a, int n);
+
+/* Comparação: retorna -1 (a < b), 0 (a == b), 1 (a > b) */
+
+/* comparação com sinal */
+int big_cmp(BigInt a, BigInt b);
+
+/* comparação sem sinal */
+int big_ucmp(BigInt a, BigInt b);
+
+#endif
