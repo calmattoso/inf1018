fafd4a664fca16c99835185c8dba96615dc91c7d
Carlos Mattoso <calmattoso@hotmail.com>
2013-08-30 21:00:44 -0300

    Implemented **big_sum** and **big_sub**. Implemented auxiliary negative function which negates a number. Tests went as expected.

--- T1/bigint.c
+++ T1/bigint.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include "bigint.h"
 
+/* Auxiliares */
+
+static void negative(BigInt res, BigInt a){
+  BigInt one;
+  int i;
+
+  for(i = 0; i < NUM_BYTES; i++)
+    res[i] = ~a[i];
+
+  big_uval(one, 1);
+
+  big_sum(res, res, one);
+}
+
 /* Atribuição */
 
 /* res = val (extensão com sinal) */
@@ -12,10 +26,10 @@ void big_val(BigInt res, int val){
     res[i] = ((val >> (8 * i)) & 0xFF);
 
   // Caso o val seja *negativo*, o padding será 0xFF
-  if(val >> 7)
+  if(val >> 31)
     padding = ~padding;
 
-  for(i = 4; i < 16; i++)
+  for(i = 4; i < NUM_BYTES; i++)
     res[i] = padding;
 }
 
@@ -26,17 +40,35 @@ void big_uval(BigInt res, unsigned int uval){
   for(i = 0; i < 4; i++)
     res[i] = ((uval >> (8 * i)) & 0xFF);
     
-  for(i = 4; i < 16; i++)
+  for(i = 4; i < NUM_BYTES; i++)
     res[i] = 0;
 }
 
 /* Operacoes aritméticas */
 
 /* res = a + b */
-void big_sum (BigInt res, BigInt a, BigInt b);
+void big_sum (BigInt res, BigInt a, BigInt b){
+  int i;
+  unsigned short sum, carry;
+
+  carry = 0;
+  for(i = 0; i < NUM_BYTES; i++){
+    sum = (unsigned short)a[i] + (unsigned short)b[i] + carry;
+
+    res[i] = (sum & 0xFF);
+
+    carry = ((sum >> 8) & 0xFF);
+  }
+}
 
 /* res = a - b */
-void big_sub (BigInt res, BigInt a, BigInt b);
+void big_sub (BigInt res, BigInt a, BigInt b){
+  BigInt neg_b;
+
+  negative(neg_b, b);
+
+  big_sum(res, a, neg_b);
+}
 
 /* res = a * b (com sinal) */
 void big_mul (BigInt res, BigInt a, BigInt b);
@@ -64,7 +96,7 @@ int big_ucmp(BigInt a, BigInt b);
 void big_print(BigInt a){
   int i;
 
-  for(i = 15; i >= 0; i--)
+  for(i = NUM_BYTES - 1; i >= 0; i--)
     printf("%02x ", a[i]);
   printf("\n");
 }
--- T1/main.c
+++ T1/main.c
@@ -2,13 +2,27 @@
 #include "bigint.h"
 
 int main(){
-  BigInt a, b;
+  BigInt a, b, res;
+  int _a, _b;
 
-  big_val(a, -25);
-  big_print(a);
 
-  big_uval(b, 1024);
-  big_print(b);
+  while(1){
+    scanf("%d %d", &_a, &_b);
+
+    big_val(a, _a);
+    big_print(a);
+
+    big_val(b, _b);
+    big_print(b);
+
+    printf("Sum\n");
+    big_sum(res, a, b);
+    big_print(res);
+
+    printf("Subtraction\n");
+    big_sub(res, a, b);
+    big_print(res);
+  }
 
   return 0;
 }
